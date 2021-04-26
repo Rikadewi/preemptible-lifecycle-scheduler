f68240777501b1a5b71edbb96b13cf34d6341e9e
rika <rikadewi4444@gmail.com>
2021-04-27 04:13:35 +0700

    Breakdown start function in scheduler

--- scheduler/client.go
+++ scheduler/client.go
@@ -3,7 +3,6 @@ package scheduler
 import (
 	corev1 "k8s.io/api/core/v1"
 	"log"
-	"preemptible-lifecycle-scheduler/cluster"
 	"preemptible-lifecycle-scheduler/peakhour"
 	"time"
 )
@@ -16,12 +15,18 @@ const (
 	StartPeakHour   = "start peak hour"
 )
 
+type ClusterClient interface {
+	GetPreemptibleNodes() (*corev1.NodeList, error)
+	ProcessNode(node corev1.Node) (err error)
+	GetNodeCreatedTime(node corev1.Node) time.Time
+}
+
 type Client struct {
-	Cluster   *cluster.Client
+	Cluster   ClusterClient
 	PeakHours *peakhour.Client
 }
 
-func NewClient(cluster *cluster.Client, peakHour *peakhour.Client) *Client {
+func NewClient(cluster ClusterClient, peakHour *peakhour.Client) *Client {
 	return &Client{
 		Cluster:   cluster,
 		PeakHours: peakHour,
@@ -49,24 +54,12 @@ func (c *Client) Start() {
 			if len(nodes.Items) == 0 {
 				continue
 			}
-
 			log.Printf("%d nodes found", len(nodes.Items))
 
-			for _, node := range nodes.Items {
-				createdAt := c.Cluster.GetNodeCreatedTime(node)
+			unprocessedNodes := c.ProcessNodesOutsidePeakHour(nodes.Items)
 
-				// node is nearly terminated
-				if createdAt.Add(24*time.Hour).Sub(peakhour.Now()) <= DefaultGracefulDuration {
-					err := c.Cluster.ProcessNode(node)
-					if err != nil {
-						log.Printf("failed to delete node: %v", err)
-						continue
-					}
-				}
-			}
-
-			log.Println("waiting for next schedule")
-			sleepDuration := c.CalculateNextSchedule(nodes.Items)
+			sleepDuration := c.CalculateNextSchedule(unprocessedNodes)
+			log.Printf("waiting for next schedule: %s", sleepDuration.String())
 			time.Sleep(sleepDuration)
 
 		case StartPeakHour:
@@ -79,28 +72,53 @@ func (c *Client) Start() {
 			if len(nodes.Items) == 0 {
 				continue
 			}
-
 			log.Printf("%d nodes found", len(nodes.Items))
 
-			for _, node := range nodes.Items {
-				createdAt := c.Cluster.GetNodeCreatedTime(node)
-				endPeakHour := c.PeakHours.GetNearestEndPeakHour()
-
-				// node won't survive next peak hour period
-				if endPeakHour.After(createdAt.Add(24 * time.Hour)) {
-					err := c.Cluster.ProcessNode(node)
-					if err != nil {
-						log.Printf("failed to delete node: %v", err)
-						continue
-					}
-				}
-			}
+			c.ProcessNodesStartPeakHour(nodes.Items)
 
 			sleepDuration := c.PeakHours.GetNearestEndPeakHour().Sub(peakhour.Now())
 			log.Printf("waiting for next peak hour period: %s", sleepDuration.String())
 			time.Sleep(sleepDuration)
 		}
 	}
+}
+
+func (c *Client) ProcessNodesStartPeakHour(nodes []corev1.Node) {
+	for _, node := range nodes {
+		createdAt := c.Cluster.GetNodeCreatedTime(node)
+		log.Println(createdAt.String())
+		endPeakHour := c.PeakHours.GetNearestEndPeakHour()
+
+		// node won't survive next peak hour period
+		if endPeakHour.After(createdAt.Add(24*time.Hour)) || endPeakHour.Equal(createdAt.Add(24*time.Hour)) {
+			err := c.Cluster.ProcessNode(node)
+			if err != nil {
+				log.Printf("failed to delete node: %v", err)
+				continue
+			}
+		}
+	}
+}
+
+func (c *Client) ProcessNodesOutsidePeakHour(nodes []corev1.Node) []corev1.Node {
+	unprocessedNodes := make([]corev1.Node, 0)
+	for _, node := range nodes {
+		createdAt := c.Cluster.GetNodeCreatedTime(node)
+		log.Println(createdAt.String())
+
+		// node is nearly terminated
+		if createdAt.Add(24*time.Hour).Sub(peakhour.Now()) <= DefaultGracefulDuration {
+			err := c.Cluster.ProcessNode(node)
+			if err != nil {
+				log.Printf("failed to delete node: %v", err)
+			}
+			continue
+		}
+
+		unprocessedNodes = append(unprocessedNodes, node)
+	}
+
+	return unprocessedNodes
 }
 
 func (c *Client) CalculateNextSchedule(nodes []corev1.Node) time.Duration {
--- scheduler/client_test.go
+++ scheduler/client_test.go
@@ -1,6 +1,9 @@
 package scheduler
 
 import (
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"preemptible-lifecycle-scheduler/cluster"
 	"preemptible-lifecycle-scheduler/peakhour"
 	"testing"
 	"time"
@@ -78,6 +81,344 @@ func TestClient_GetPeakHourState(t *testing.T) {
 			client := NewClient(nil, ph)
 			if client.GetPeakHourState() != tc.Expected {
 				t.Errorf("expected %v, got %v", tc.Expected, client.GetPeakHourState())
+			}
+		})
+	}
+}
+
+func TestClient_CalculateNextSchedule(t *testing.T) {
+	tests := map[string]struct {
+		PeriodStr     []string
+		NodeCreatedTs []time.Time
+		CurrentTime   time.Time
+		Expected      time.Duration
+	}{
+		"next peak hour": {
+			PeriodStr: []string{"11:00-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 10, 22, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 1, 10, 23, 0, 0, time.Now().Location()),
+			Expected:    7 * time.Minute,
+		},
+		"one node": {
+			PeriodStr: []string{"12:00-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 11, 30, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 10, 23, 0, 0, time.Now().Location()),
+			Expected:    37 * time.Minute,
+		},
+		"many nodes": {
+			PeriodStr: []string{"12:00-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 11, 30, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 12, 45, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 10, 21, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 00, 00, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 10, 23, 0, 0, time.Now().Location()),
+			Expected:    37 * time.Minute,
+		},
+		"next period midnight": {
+			PeriodStr: []string{"00:35-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 11, 30, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 12, 45, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 21, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 57, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+			Expected:    6 * time.Minute,
+		},
+		"node midnight": {
+			PeriodStr: []string{"12:00-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 11, 30, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 12, 45, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 21, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 00, 35, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+			Expected:    6 * time.Minute,
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			peakhour.Now = func() time.Time {
+				return tc.CurrentTime
+			}
+
+			ph, err := peakhour.NewClient(tc.PeriodStr)
+			if err != nil {
+				t.Errorf("failed to create peak hour client %v", err)
+			}
+
+			nodes := make([]corev1.Node, 0)
+			for _, ts := range tc.NodeCreatedTs {
+				nodes = append(nodes, corev1.Node{
+					ObjectMeta: v1.ObjectMeta{
+						CreationTimestamp: v1.Time{Time: ts},
+					},
+				})
+			}
+
+			client := NewClient(&cluster.Client{}, ph)
+			if client.CalculateNextSchedule(nodes) != tc.Expected {
+				t.Errorf("expected %v, got %v", tc.Expected, client.CalculateNextSchedule(nodes))
+			}
+		})
+	}
+}
+
+type MockClusterClient struct {
+	ProcessedTs []time.Time
+}
+
+func NewMockClusterClient() *MockClusterClient {
+	return &MockClusterClient{
+		ProcessedTs: make([]time.Time, 0),
+	}
+}
+
+func (c *MockClusterClient) GetPreemptibleNodes() (*corev1.NodeList, error) {
+	return nil, nil
+}
+
+func (c *MockClusterClient) ProcessNode(node corev1.Node) (err error) {
+	c.ProcessedTs = append(c.ProcessedTs, c.GetNodeCreatedTime(node))
+	return nil
+}
+
+func (c *MockClusterClient) GetNodeCreatedTime(node corev1.Node) time.Time {
+	cc := &cluster.Client{}
+	return cc.GetNodeCreatedTime(node)
+}
+
+func TestClient_ProcessNodesStartPeakHour(t *testing.T) {
+	tests := map[string]struct {
+		PeriodStr     []string
+		NodeCreatedTs []time.Time
+		CurrentTime   time.Time
+		Expected      []time.Time
+	}{
+		"normal case": {
+			PeriodStr: []string{"09:00-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 10, 22, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 8, 15, 0, 0, time.Now().Location()),
+			Expected: []time.Time{
+				time.Date(1, 1, 1, 10, 22, 0, 0, time.Now().Location()),
+			},
+		},
+		"normal case, empty": {
+			PeriodStr: []string{"09:00-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 15, 01, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 8, 15, 0, 0, time.Now().Location()),
+			Expected:    []time.Time{},
+		},
+		"equal": {
+			PeriodStr: []string{"09:00-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 15, 00, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 8, 15, 0, 0, time.Now().Location()),
+			Expected: []time.Time{
+				time.Date(1, 1, 1, 15, 00, 0, 0, time.Now().Location()),
+			},
+		},
+		"more nodes": {
+			PeriodStr: []string{"09:00-15:00"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 15, 00, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 9, 00, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 8, 50, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 50, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 8, 30, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 0, 0, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 17, 29, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 8, 45, 0, 0, time.Now().Location()),
+			Expected: []time.Time{
+				time.Date(1, 1, 1, 15, 00, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 9, 00, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 8, 50, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 50, 0, 0, time.Now().Location()),
+			},
+		},
+		"mid night": {
+			PeriodStr: []string{"00:00-04:00", "23:55-23:59"},
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 2, 15, 00, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 56, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 00, 00, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 4, 01, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 2, 35, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 23, 45, 0, 0, time.Now().Location()),
+			Expected: []time.Time{
+				time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 00, 00, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 2, 35, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 56, 0, 0, time.Now().Location()),
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			peakhour.Now = func() time.Time {
+				return tc.CurrentTime
+			}
+
+			ph, err := peakhour.NewClient(tc.PeriodStr)
+			if err != nil {
+				t.Errorf("failed to create peak hour client %v", err)
+			}
+
+			nodes := make([]corev1.Node, 0)
+			for _, ts := range tc.NodeCreatedTs {
+				nodes = append(nodes, corev1.Node{
+					ObjectMeta: v1.ObjectMeta{
+						CreationTimestamp: v1.Time{Time: ts},
+					},
+				})
+			}
+
+			cc := NewMockClusterClient()
+			client := NewClient(cc, ph)
+			client.ProcessNodesStartPeakHour(nodes)
+
+			if !isTimestampsEqual(cc.ProcessedTs, tc.Expected) {
+				t.Errorf("expected %v, got %v", tc.Expected, cc.ProcessedTs)
+			}
+		})
+	}
+}
+
+func isTimestampsEqual(t1 []time.Time, t2 []time.Time) bool {
+	if len(t1) != len(t2) {
+		return false
+	}
+
+	exist := make(map[time.Time]struct{}, 0)
+	for _, t := range t1 {
+		exist[t] = struct{}{}
+	}
+
+	for _, t := range t2 {
+		if _, ok := exist[t]; !ok {
+			return false
+		}
+	}
+
+	return true
+}
+
+func TestClient_ProcessNodesOutsidePeakHour(t *testing.T) {
+	tests := map[string]struct {
+		NodeCreatedTs       []time.Time
+		CurrentTime         time.Time
+		ProcessedExpected   []time.Time
+		UnprocessedExpected []time.Time
+	}{
+		"normal case": {
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 10, 22, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 10, 15, 0, 0, time.Now().Location()),
+			ProcessedExpected: []time.Time{
+				time.Date(1, 1, 1, 10, 22, 0, 0, time.Now().Location()),
+			},
+			UnprocessedExpected: []time.Time{},
+		},
+		"many nodes": {
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 10, 22, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 45, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 15, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 46, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 10, 14, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 0, 0, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 10, 15, 0, 0, time.Now().Location()),
+			ProcessedExpected: []time.Time{
+				time.Date(1, 1, 1, 10, 22, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 45, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 10, 15, 0, 0, time.Now().Location()),
+			},
+			UnprocessedExpected: []time.Time{
+				time.Date(1, 1, 1, 10, 46, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 10, 14, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 0, 0, 0, 0, time.Now().Location()),
+			},
+		},
+		"mid night": {
+			NodeCreatedTs: []time.Time{
+				time.Date(1, 1, 1, 23, 58, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 0, 0, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 0, 5, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 23, 50, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 1, 50, 0, 0, time.Now().Location()),
+			},
+			CurrentTime: time.Date(1, 1, 2, 23, 55, 0, 0, time.Now().Location()),
+			ProcessedExpected: []time.Time{
+				time.Date(1, 1, 1, 23, 58, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 1, 23, 59, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 0, 0, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 0, 5, 0, 0, time.Now().Location()),
+			},
+			UnprocessedExpected: []time.Time{
+				time.Date(1, 1, 2, 23, 50, 0, 0, time.Now().Location()),
+				time.Date(1, 1, 2, 1, 50, 0, 0, time.Now().Location()),
+			},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			peakhour.Now = func() time.Time {
+				return tc.CurrentTime
+			}
+
+			ph, err := peakhour.NewClient([]string{})
+			if err != nil {
+				t.Errorf("failed to create peak hour client %v", err)
+			}
+
+			nodes := make([]corev1.Node, 0)
+			for _, ts := range tc.NodeCreatedTs {
+				nodes = append(nodes, corev1.Node{
+					ObjectMeta: v1.ObjectMeta{
+						CreationTimestamp: v1.Time{Time: ts},
+					},
+				})
+			}
+
+			cc := NewMockClusterClient()
+			client := NewClient(cc, ph)
+
+			unprocessedNodes := client.ProcessNodesOutsidePeakHour(nodes)
+			unprocessedTs := make([]time.Time, 0)
+			for _, node := range unprocessedNodes {
+				unprocessedTs = append(unprocessedTs, cc.GetNodeCreatedTime(node))
+			}
+
+			if !isTimestampsEqual(cc.ProcessedTs, tc.ProcessedExpected) {
+				t.Errorf("processed timstamp expected %v, got %v", tc.ProcessedExpected, cc.ProcessedTs)
+			}
+
+			if !isTimestampsEqual(unprocessedTs, tc.UnprocessedExpected) {
+				t.Errorf("unprocessed timestamp expected %v, got %v", tc.UnprocessedExpected, unprocessedTs)
 			}
 		})
 	}
